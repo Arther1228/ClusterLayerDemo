d4f5abbcda16babaaf3fcae5b660176191acd15e
Arther1228 <396986654@qq.com>
2017-08-08 14:31:20 +0800

    no-AMD III 基本都是 leavy

--- WebRoot/js/Map_Fun.js
+++ WebRoot/js/Map_Fun.js
@@ -16,23 +16,23 @@ function addClusters(resp) {
       "wkid": 4326
     });
     
-    require(["dojo/_base/array"], function(arrayUtils) {
-        photoInfo.data = arrayUtils.map(resp, function(p) {
-            var latlng = new esri.geometry.Point(parseFloat(p.lng), parseFloat(p.lat), wgs);
-            var webMercator = new esri.geometry.geographicToWebMercator(latlng);
-            var attributes = {
-              "Caption": p.caption,
-              "Name": p.full_name,
-              "Image": p.image,
-              "Link": p.link
-            };
-            return {
-              "x": webMercator.x,
-              "y": webMercator.y,
-              "attributes": attributes
-            };
-          });
-    	});
+    dojo.require("dojo._base.array");
+    
+    photoInfo.data = dojo._base.array.map(resp, function(p) {
+        var latlng = new esri.geometry.Point(parseFloat(p.lng), parseFloat(p.lat), wgs);
+        var webMercator = new esri.geometry.geographicToWebMercator(latlng);
+        var attributes = {
+          "Caption": p.caption,
+          "Name": p.full_name,
+          "Image": p.image,
+          "Link": p.link
+        };
+        return {
+          "x": webMercator.x,
+          "y": webMercator.y,
+          "attributes": attributes
+        };
+      });
     
     // popupTemplate to work with attributes specific to this dataset
     var popupTemplate = new esri.dijit.PopupTemplate({
@@ -51,18 +51,17 @@ function addClusters(resp) {
       }]
     });
     
-    // cluster layer that uses OpenLayers style clustering
-    require(["extras/ClusterLayer"], function(ClusterLayer){
-    	clusterLayer = new ClusterLayer({
-	      "data": photoInfo.data,
-	      "distance": 100,
-	      "id": "clusters",
-	      "labelColor": "#fff",
-	      "labelOffset": 10,
-	      "resolution": map.extent.getWidth() / map.width,
-	      "singleColor": "#888",
-	      "singleTemplate": popupTemplate
-	    });
+    dojo.require("extras.ClusterLayer");
+
+    clusterLayer = new extras.ClusterLayer({
+      "data": photoInfo.data,
+      "distance": 100,
+      "id": "clusters",
+      "labelColor": "#fff",
+      "labelOffset": 10,
+      "resolution": map.extent.getWidth() / map.width,
+      "singleColor": "#888",
+      "singleTemplate": popupTemplate
     });
    
     renderer.addBreak(0, 2, blue);
